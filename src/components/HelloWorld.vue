8b72035c922f6674c31bbee6fe2202c3ef73b561
shenzhun <shenzhunallen@gmail.com>
2018-03-24 19:22:38 +0800

    add description and content links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div id="container">
+    <div id="header">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div id="content" class="episodes">
+      <div class="episode">
+        <h2>Episode 1: Hello World! </h2>
+        <audio controls style="width: 50%;">
+          <source src="../assets/Hardcoded_E01_Hello_World.mp3" type="audio/mpeg">
+        </audio>
+      </div>
+        <div class="content_list">
+          <ul>
+            <li>
+              <article>
+                Hardcoded 表示工程师辛勤写代码，为什么最后又在后面加上了d呢? 当大家写完了代码，自己挖了坑，自己的狗粮自己吃了之后, 聊过一些创业的辛酸苦辣，写代码做产品，努力寻找方向, 同时HBO《硅谷》第5季25号就要开播了，有很多有意思技术梗，程序员鄙视链，同时也真实的反应了创业公司的辛酸，刺激和无奈. Stay tuned.
+              </article>
+            </li>
+            <br>
+            <li>
+              <a
+                href="https://www.hbo.com/silicon-valley"
+                target="_blank"
+              >q
+                Silicon Valley
+              </a>
+            </li>
+            <br>
+            <li>
+              <a
+                href="https://www.amazon.com/Hard-Thing-About-Things-Building/dp/0062273205"
+                target="_blank"
+              >
+                The Hard Thing About Hard Things
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Hardcoded',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'a podcast for hard coders'
     }
   }
 }
@@ -96,7 +55,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1, h2, h3 {
   font-weight: normal;
 }
 ul {
@@ -110,4 +69,14 @@ li {
 a {
   color: #42b983;
 }
+
+.episode {
+}
+
+.content_list {
+  display: inline-table;
+  width: 50%;
+  text-align: left;
+}
+
 </style>
